d12f313f7776ef4fe0e8ace5d5a1065149c6a847
Sumit <ssingh7296003817@gmail.com>
2024-03-06 21:12:53 +0530

    First commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
  
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello from Koyeb'
  
  
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
